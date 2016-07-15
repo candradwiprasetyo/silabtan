e8f4a75b79716459df8cd8fd77f824f28c47bcf3
Candra Dwi Prasetyo <candradwiprasetyo@gmail.com>
2016-07-15 09:41:44 +0700

    nw

--- template/pesan.php
+++ template/pesan.php
@@ -1,7 +1,9 @@
+
+
 <div class="pageheader">
       <h2><i class="fa fa-envelope"></i> Pesan</h2>
       <div class="breadcrumb-wrapper">
-        
+        <a href="<?php echo($base_url."pesan"); ?>" class="btn btn-primary">New Chat</a>
       </div>
     </div>
     
@@ -10,16 +12,12 @@
 
         <div class="row">
             <div class="col-sm-4 col-lg-3">
-               
-                
-                
-               
                 <ul class="nav nav-pills nav-stacked nav-email mb20">
                	<?php
                	$query_user = mysql_query("select * from tsilabtan_user where username <> '".$_SESSION['username']."'");
                	while($row_user = mysql_fetch_array($query_user)){
                	?>
-                <li><a href="<?php echo($base_url."pesan&chat=".$row_user['USERNAME']); ?>"><i class="fa fa-user"></i><?= $row_user['FULLNAME'] ?></a></li>
+                <li <?php if($row_user['USERNAME']==@$_GET['chat']){ ?>class="active"<?php } ?>><a href="<?php echo($base_url."pesan&chat=".$row_user['USERNAME']); ?>"><i class="fa fa-user"></i><?= $row_user['FULLNAME'] ?></a></li>
                	<?php
                	}
                	?>
@@ -35,18 +33,22 @@
                         <div class="table-responsive">
                             <table class="table table-email">
                               <tbody>
-                              	 	<?php
-                              	 	$chat = (isset($_GET['chat'])) ? $_GET['chat'] : "";
-                              	 	if($chat){
+                                
 
-					               	$query_chat = mysql_query("select a.*, b.* from tsilabtan_message a
-					               								join tsilabtan_user b on b.USERNAME = a.USERNAME
-					               								where (a.USERNAME = '".$_SESSION['username']."' and a.TSI_USERNAME = '".$_GET['chat']."') 
-					               								or (a.TSI_USERNAME = '".$_SESSION['username']."' and a.USERNAME = '".$_GET['chat']."')
-					               								order by IDMESSAGE asc
-					               								");
-					               	while($row_chat = mysql_fetch_array($query_chat)){
-					               	?>
+                                <?php
+                                  $chat = (isset($_GET['chat'])) ? $_GET['chat'] : "";
+                                  if($chat){
+                                    ?>
+                                    <?php
+
+                          $query_chat = mysql_query("select a.*, b.* from tsilabtan_message a
+                                        join tsilabtan_user b on b.USERNAME = a.USERNAME
+                                        where (a.USERNAME = '".$_SESSION['username']."' and a.TSI_USERNAME = '".$_GET['chat']."') 
+                                        or (a.TSI_USERNAME = '".$_SESSION['username']."' and a.USERNAME = '".$_GET['chat']."')
+                                        order by IDMESSAGE asc
+                                        ");
+                          while($row_chat = mysql_fetch_array($query_chat)){
+                          ?>
                                 <tr class="unread">
                                   <td>
                                     <div class="media">
@@ -57,68 +59,83 @@
                                             <span class="media-meta pull-right"><?= $row_chat['DATECREATED'] ?></span>
                                             <h4 class="text-primary"><?= $row_chat['FULLNAME'] ?></h4>
                                             <small class="text-muted"></small>
+                                            <?php
+                                            if($row_chat['ATTACHMENT']){
+                                              ?>
+                                              <img src="<?= $row_chat['ATTACHMENT'] ?>" width="200">
+                                              <?php
+                                            }
+                                            ?>
                                             <p class="email-summary"><?= $row_chat['CONTENTOFMESSAGE'] ?></p>
                                         </div>
                                     </div>
                                   </td>
                                 </tr>
                                 <?php
-                            		}
-                            	
+                                }
+                              
                                 ?>
                                 <tr>
-                                	<td>
-                                		<form method="post" action="proses_pesan.php?act=comment&tsi_username=<?= $_GET['chat']?>" class="form-horizontal form-bordered" enctype="multipart/form-data">
-			
-                                		<input type="text" class="form-control" name="i_comment"></input>
-                                		</form>
-                                	</td>
+                                  <td>
+                                    <form method="post" action="proses_pesan.php?act=comment&tsi_username=<?= $_GET['chat']?>" class="form-horizontal form-bordered" enctype="multipart/form-data">
+      
+                                    <input type="text" class="form-control" name="i_comment"></input>
+                                    </form>
+                                  </td>
                                 </tr>
                                 <?php
                             }else{ 
                                 ?>
-                                	
+                                  
 
                                 <form method="post" action="proses_pesan.php?act=new_comment" class="form-horizontal form-bordered" enctype="multipart/form-data">
-				<div class="panel panel-primary">
-					<div class="panel-body">
-						
-						<div class="form-group">
-							<label class="col-sm-3 control-label">User</label>
-							<div class="col-sm-5">
-								<select name="i_new_user" class="form-control chosen-select" data-placeholder="Pilih User...">
-									<option name=""></option>
-									<?php 
-									$q_user=mysql_query("SELECT * FROM tsilabtan_user where USERNAME <> '".$_SESSION['username']."'");
-									while($r_user=mysql_fetch_array($q_user)){
-									?>
-									<option value="<?php echo $r_user['USERNAME'];?>"><?php echo $r_user['FULLNAME'];?></option>
-									<?php
-									}
-									?>
-									</select>
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-sm-3 control-label">Comment</label>
-							<div class="col-sm-6">
-								<textarea name="i_new_comment" placeholder="Start conversation" class="form-control" rows="5"></textarea>
-							</div>
-						</div>
-					
-					</div><!-- panel-body -->
-					<div class="panel-footer">
-						<center>
-							<input type="submit" class="btn btn-primary" value="Sent" ></button>
-							
-						</center>
-					</div><!-- panel-footer -->
-				</div><!-- panel -->
-			</form>
+        <div class="panel panel-primary">
+          <div class="panel-body">
+            
+            <div class="form-group">
+              <label class="col-sm-3 control-label">User</label>
+              <div class="col-sm-5">
+                <select name="i_new_user" class="form-control chosen-select" data-placeholder="Pilih User...">
+                  <option name=""></option>
+                  <?php 
+                  $q_user=mysql_query("SELECT * FROM tsilabtan_user where USERNAME <> '".$_SESSION['username']."'");
+                  while($r_user=mysql_fetch_array($q_user)){
+                  ?>
+                  <option value="<?php echo $r_user['USERNAME'];?>"><?php echo $r_user['FULLNAME'];?></option>
+                  <?php
+                  }
+                  ?>
+                  </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label">Comment</label>
+              <div class="col-sm-6">
+                <textarea name="i_new_comment" placeholder="Start conversation" class="form-control" rows="5"></textarea>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label">Gambar</label>
+              <div class="col-sm-6">
+               <input type="file" name="i_img">
+              </div>
+            </div>
+          
+          </div><!-- panel-body -->
+          <div class="panel-footer">
+            <center>
+              <input type="submit" class="btn btn-primary" value="Sent" />
+              
+            </center>
+          </div><!-- panel-footer -->
+        </div><!-- panel -->
+      </form>
 
                                 <?php
-								}
+                }
                                 ?>
+                                  
+
                               </tbody>
                             </table>
                         </div><!-- table-responsive -->
--- template/proses_pesan.php
+++ template/proses_pesan.php
@@ -12,8 +12,18 @@ switch ($act) {
 	break;
 	
 	case 'new_comment':
+		$img = ($_FILES['i_img']['name']) ? time().$_FILES['i_img']['name'] : null;
+		$i_img_tmp = $_FILES['i_img']['tmp_name'];
+		$path = "./uploads/";
+
+		if($img){
+			move_uploaded_file($i_img_tmp, "uploads/".$img);
+			$new_img = $path.$img;
+		}else{
+			$new_img = '';
+		}
 		mysql_query("insert into tsilabtan_message values 
-			('','".$_SESSION['username']."', '".$_POST['i_new_user']."', '".$_POST['i_new_comment']."', '', '".date("Y-m-d H:i:s")."')");	
+			('','".$_SESSION['username']."', '".$_POST['i_new_user']."', '".$_POST['i_new_comment']."', '$new_img', '".date("Y-m-d H:i:s")."')");	
 		header("Location: index.php?page=pesan&chat=$_POST[i_new_user]");
 	break;
 }
